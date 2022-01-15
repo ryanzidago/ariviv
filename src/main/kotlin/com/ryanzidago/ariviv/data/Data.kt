3a4bc4d4373fbc4b1e5ede58c6bcbb610c9678bd
Ryan Zidago <ryanzidago@gmail.com>
2022-01-15 18:38:17 +0100

    added seed data

---
+++ src/main/kotlin/com/ryanzidago/ariviv/data/Data.kt
@@ -0,0 +1,9 @@
+package com.ryanzidago.ariviv.data
+
+import com.ryanzidago.ariviv.domain_models.User
+
+val users = mutableListOf<User>(
+    User("Jean", "jean@email.fr"),
+    User("Günther", "guenther@email.de"),
+    User("Julie", "julie@email.fr")
+)
