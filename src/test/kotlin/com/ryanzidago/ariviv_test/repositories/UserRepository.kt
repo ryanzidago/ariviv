3156d0cfbc03237680d922e45b6a043bdfa6eb16
Ryan Zidago <ryanzidago@gmail.com>
2022-01-15 18:42:36 +0100

    improved testing of UserRepository

--- src/test/kotlin/com/ryanzidago/ariviv_test/repositories/UserRepository.kt
+++ src/test/kotlin/com/ryanzidago/ariviv_test/repositories/UserRepository.kt
@@ -2,13 +2,14 @@ package com.ryanzidago.ariviv_test.repositories
 
 import com.ryanzidago.ariviv.data.users
 import com.ryanzidago.ariviv.domain_models.User
+import com.ryanzidago.ariviv.repositories.UserRepository
 import kotlin.test.Test
 
 class UserRepositoryTest {
     @Test
     fun createUserTest() {
         val user = User("Hyacinthe", "hyacinthe@email.fr")
-        users.add(user)
+        UserRepository().createUser(user)
         assert(users.contains(user))
     }
 }   
