a6fbbf55ace064e3694c91963da128a09537fe25
Ryan Zidago <ryanzidago@gmail.com>
2022-01-15 18:28:05 +0100

    added user domain model

---
+++ src/main/kotlin/com/ryanzidago/ariviv/domain_models/User.kt
@@ -0,0 +1,3 @@
+package com.ryanzidago.ariviv.domain_models
+
+data class User(val name: String, val email: String)
---
+++ src/test/kotlin/com/ryanzidago/ariviv_test/domain_models/UserTest.kt
@@ -0,0 +1,16 @@
+package com.ryanzidago.ariviv_test.domain_models
+
+import org.junit.Test
+import kotlin.test.assertEquals
+import com.ryanzidago.ariviv.domain_models.User
+
+
+class UserTest {
+    @Test
+    fun userInstanceCanBeCreated() {
+        val user = User("Ryan",  "ryan@email.fr")
+        assertEquals(user.name, "Ryan")
+        assertEquals(user.email, "ryan@email.fr")
+    }
+}
+
