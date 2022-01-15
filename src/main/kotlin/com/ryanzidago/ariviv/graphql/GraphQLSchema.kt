f772acda821fbf14da07e4911768a5391a75c3e4
Ryan Zidago <ryanzidago@gmail.com>
2022-01-15 19:21:12 +0100

    updated GraphQL schema to create users

--- src/main/kotlin/com/ryanzidago/ariviv/graphql/GraphQLSchema.kt
+++ src/main/kotlin/com/ryanzidago/ariviv/graphql/GraphQLSchema.kt
@@ -1,13 +1,29 @@
 package com.ryanzidago.ariviv.graphql
 
 import com.apurebase.kgraphql.schema.dsl.SchemaBuilder
+import com.ryanzidago.ariviv.application_services.RegisterUserService
 import com.ryanzidago.ariviv.repositories.UserRepository
 
 fun SchemaBuilder.schemaValue() {
     query("users") {
         description = "Retrieves all users"
-        resolver {
-            -> UserRepository().listUsers() ?: throw Exception("Could not list users")
+        resolver { -> UserRepository().listUsers() ?: throw Exception("Could not list users")
+        }
+    }
+
+    mutation("registerUser") {
+        description = "Registers a user"
+        resolver { name: String, email: String  ->
+          RegisterUserService().perform(name, email) ?: throw Exception("User could not be created")
+        }.withArgs {
+            arg<String> {
+                name = "name"
+                description = "The name of the user"
+            }
+            arg<String> {
+                name = "email"
+                description = "The email address of the user"
+            }
         }
     }
 }
