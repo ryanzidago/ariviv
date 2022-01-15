fbb57dc64b2148a61ef170568a31c319b951b2de
Ryan Zidago <ryanzidago@gmail.com>
2022-01-15 20:26:01 +0100

    added types to domain events

--- src/main/kotlin/com/ryanzidago/ariviv/domain_events/domain_event.kt
+++ src/main/kotlin/com/ryanzidago/ariviv/domain_events/domain_event.kt
@@ -2,6 +2,8 @@ package com.ryanzidago.ariviv.domain_events
 
 enum class DomainEventType {
     UserRegistered,
+    UserEnrolledInExerciseRoutine,
+    ExerciseSessionMarkedAsFinished,
 }
 
 data class DomainEvent(val type: DomainEventType, val payload: HashMap<Any, Any>)
